cf919c981e03fb414099a4eb572fb2449f7185c2
dabour1 <90671017+dabour1@users.noreply.github.com>
2021-11-18 19:51:42 +0200

    Update myhead.js

--- src/compo/myhead.js
+++ src/compo/myhead.js
@@ -1,11 +1,20 @@
 
 import React from 'react';
+import Mybook from './mybooks';
 
-const Myhead=()=>{
+function Cover({ book,moveshelf }){
+  
     return(
-        
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>)
+      <div className="book">
+          <div className="book-top">
+      <div className="book-cover" style={{ width: 128, height: 193, 
+        backgroundImage: `url(${book.imageLinks ? book.imageLinks.thumbnail :""})` }}></div>
+        <Mybook book={book} moveshelf={moveshelf} />
+                 </div>
+               
+                 <div className="book-title">{book.title}</div>
+          <div className="book-authors">{book.authors}</div>
+          </div>
+        )
 }
-export default Myhead
+export default Cover
